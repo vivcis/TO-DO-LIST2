f9768cb9d446fc2ccaaeaf956621d06f980b17b4
vivcis <ceciliaorji.co@gmail.com>
2022-07-03 22:39:47 +0100

    added the delete functionalities

--- app.js
+++ app.js
@@ -4,17 +4,28 @@ let newTask = document.getElementById('new-task');
 let done = document.getElementById('done');
 let cancel = document.getElementById('cancel');
 
-inputContainerButton.addEventListener('click', function(e){
+inputContainerButton.addEventListener('click', function (e) {
 	e.preventDefault();
 	var list = document.createElement('li')
 	list.classList.add('list-styling');
+
 	list.innerText = newTask.value;
-	toDoContainer.appendChild(list);
-	newTask.value = "";
-	done.addEventListener('click', function(){
-		done.style.textDecoration = 'line-through';
-	})
-	cancel.addEventListener('click', function(){
-		toDoContainer.removeChild(cancel):
-	})
+	if(list.innerText===""){
+		alert('Please input text')
+	}else{
+		toDoContainer.appendChild(list);
+	}
+	var liNumber = document.querySelectorAll("ol li").length
+	if (liNumber === 6) {
+		alert('You have reached the maximum number of entries');
+		toDoContainer.removeChild(list);
+	}
+		newTask.value = "";
 })
+
+cancel.addEventListener('click', function (e) {
+	e.preventDefault();
+	const item = toDoContainer.lastChild;
+	item.parentElement.removeChild(item);
+})
+
